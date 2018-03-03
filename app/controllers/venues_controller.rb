f118e5e575bf7a50c60245f58152d62ebcd8b27d
Jeff <endymionjkb@gmail.com>
2018-03-03 15:39:29 -0500

    Add api calls

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,5 +1,23 @@
 class VenuesController < ApplicationController
+  respond_to :html, :json
+  
   def index
     @venues = Venue.order(:name).paginate(:page => params[:page])
+
+    data = []
+    @venues.each do |v|
+      data.push({:id => v.id, :name => v.name, :neighborhood => v.neighborhood, :latitude => v.latitude, :longitude => v.longitude, :url => v.url})
+    end
+    
+    respond_to do |format|
+      format.html
+      format.json { render :json => data }
+    end    
+  end
+  
+  def show
+    @venue = Venue.find(params[:id])
+    
+    render :json => @venue
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -22,3 +22,34 @@ if 0 == Genre.count
   Genre.create!(:name => "Spiritual")
   Genre.create!(:name => "World Music")
  end
+ 
+if 0 == Artist.count
+  Artist.create!(:name => "Joe Satriani", :is_mentor => true, :genre_id => Genre.find_by_name("Rock").id)
+  Artist.create!(:name => "Metallica", :is_mentor => true, :genre_id => Genre.find_by_name("Metal").id)
+  Artist.create!(:name => "Modest Mouse", :is_mentor => false, :genre_id => Genre.find_by_name("Rock").id)
+  Artist.create!(:name => "Britney Spears", :is_mentor => false, :genre_id => Genre.find_by_name("Pop").id)
+  Artist.create!(:name => "Kenny Chesney", :is_mentor => false, :genre_id => Genre.find_by_name("Country").id)
+  Artist.create!(:name => "Donnie Iris", :is_mentor => false, :genre_id => Genre.find_by_name("Folk").id)
+end
+
+if 0 == Event.count
+  Event.create!(:artist_id => Artist.find_by_name('Metallica').id, :venue_id => Venue.find_by_name('61B Cafe').id, 
+                :date => 10.days.from_now, :title => 'Metallica at 61B', :description => 'This is a description',
+                :price_advance => 150, :price_door => 200, :all_ages => false)
+
+  Event.create!(:artist_id => Artist.find_by_name('Joe Satriani').id, :venue_id => Venue.find_by_name('12 Peers Theater').id, 
+                :date => 11.days.from_now, :title => 'Crazy Joe!', :description => 'This is another description',
+                :price_advance => 150, :price_door => 200, :all_ages => true)
+
+  Event.create!(:artist_id => Artist.find_by_name('Modest Mouse').id, :venue_id => Venue.find_by_name('1311').id, 
+                :date => 30.days.from_now, :title => 'Mouse in the house!', :description => 'This is Modest Mouse',
+                :price_advance => 15, :price_door => 230, :all_ages => true)
+
+  Event.create!(:artist_id => Artist.find_by_name('Britney Spears').id, :venue_id => Venue.find_by_name('15 Minutes Gallery').id, 
+                :date => 10.month.from_now, :title => "It's Britney, bitches!", :description => 'This is another description',
+                :price_advance => 20, :price_door => 50, :all_ages => false)
+
+  Event.create!(:artist_id => Artist.find_by_name('Donnie Iris').id, :venue_id => Venue.find_by_name('1613 Saloon').id, 
+                :date => 1.year.from_now, :title => 'Back in da Burgh!', :description => 'Go Dahntahn yinz!!',
+                :price_advance => 15, :price_door => 20, :all_ages => true)
+end
