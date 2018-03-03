e62f5bdadcddc31aef0e2c41fdeada274f64d56b
Jeff <endymionjkb@gmail.com>
2018-03-03 15:55:34 -0500

    Add image to artists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303193337) do
+ActiveRecord::Schema.define(version: 20180303204346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20180303193337) do
     t.datetime "updated_at", null: false
     t.boolean "is_mentor"
     t.integer "genre_id"
+    t.string "image_url"
   end
 
   create_table "events", force: :cascade do |t|
